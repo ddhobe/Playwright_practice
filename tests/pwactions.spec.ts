c19ac783e892fd992c69cfeb7fef63a181f9191a
ddhobe <dattadhobe1206@gmail.com>
2026-03-28 15:46:32 +0530

    commit 5

--- tests/pwactions.spec.ts
+++ tests/pwactions.spec.ts
@@ -13,3 +13,21 @@ test("playwright actions-input Box",async({page})=>{
     console.log("textBox input value is:", enteredValue);
      expect(enteredValue).toBe("John Kenedy")
 })
+
+
+//radio buttons
+
+test("Radio buttons",async({page})=>{
+    page.goto("https://testautomationpractice.blogspot.com/");
+    const maleRadio:Locator= page.locator('#male');
+    await expect(maleRadio).toBeVisible();
+    await expect (maleRadio).toBeEnabled();
+
+    //select radio button
+  await maleRadio.check();
+  // validation
+  await expect(maleRadio).toBeChecked();
+
+})
+
+
